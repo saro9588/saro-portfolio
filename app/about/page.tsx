a07186d2421a6f2c55cb1f37e0d21b293aa098ec
Saro <babikiansaro@gmail.com>
2023-12-22 12:03:57 -0800

    add &apos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,11 +7,14 @@ const Page = () => {
         <p>
           I am a LA based software developer with experience in HTML, CSS,
           JavaScript, and the MERN stack. I am a recent graduate of the MIT xPro
-          Full-Stack Software Development bootcamp. I've built apps and websites
-          using the MERN stack. In addition, I create and host WordPress
-          websites. As a musician, I had the honor to perform at the prestigious
-          Carnegie Hall in NY in 2019. In addition, I am the founder and the
-          manager of Saro's Guitar Studio.
+          Full-Stack Software Development bootcamp. I have built apps and
+          websites using the MERN stack. In addition, I create and host
+          WordPress websites.
+        </p>
+        <p>
+          As a musician, I had the honor to perform at the prestigious Carnegie
+          Hall in NY in 2019. In addition, I am the founder and the manager of
+          Saro&apos;s Guitar Studio.
         </p>
       </div>
     </div>
