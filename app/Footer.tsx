ad9739d6957b0963c6f40a2d57147417bef50f1d
Saro <babikiansaro@gmail.com>
2023-12-22 11:55:09 -0800

    add bio & style footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,12 +2,21 @@ import { Divider } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+import { CiPhone } from "react-icons/ci";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <footer className="border-t px-5 py-3">
+      <footer className="border-t px-5 py-5">
         <div className="flex h-7 justify-center items-center space-x-5">
+          <div className="flex justify-center items-center space-x-5">
+            <Link href="tel:+18187266444">
+              <CiPhone />
+            </Link>
+            <Link href="tel:+18187266444">+1-818-726-6444</Link>
+          </div>
+
           <Divider orientation="vertical" />
           <Link href="https://github.com/saro9588">
             <FaGithub />
@@ -17,6 +26,10 @@ const Footer = () => {
             <FaLinkedin />
           </Link>
           <Divider orientation="vertical" />
+          <div className="flex justify-left items-center space-x-5">
+            <MdOutlineMail />
+            babikiansaro@gmail.com
+          </div>
         </div>
       </footer>
     </div>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const page = () => {
-  return <div>Bio</div>;
+const Page = () => {
+  return (
+    <div className="page-container">
+      <div className="text-container">
+        <p>
+          I am a LA based software developer with experience in HTML, CSS,
+          JavaScript, and the MERN stack. I am a recent graduate of the MIT xPro
+          Full-Stack Software Development bootcamp. I've built apps and websites
+          using the MERN stack. In addition, I create and host WordPress
+          websites. As a musician, I had the honor to perform at the prestigious
+          Carnegie Hall in NY in 2019. In addition, I am the founder and the
+          manager of Saro's Guitar Studio.
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
