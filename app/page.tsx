e03b234bb6c350eaeb1c9e321f0d90c6dffabf0d
Saro <babikiansaro@gmail.com>
2023-12-23 15:48:41 -0800

    style text in dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@ import { Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center text-center h-full">
+    <main className="flex justify-center items-center text-center h-full dark:text-white">
       <div>
         <h1>Saro&apos;s Portfolio</h1>
         <Divider className="my-4" />
-
         <div className="my-20">
           <p>LA based software developer.</p>
         </div>
