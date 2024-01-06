b550e1c18a5256ff9216547e1249f4662dea4058
Saro <babikiansaro@gmail.com>
2024-01-05 16:48:54 -0800

    style footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -9,36 +9,36 @@ import { ThemeSwitcher } from "./ThemeSwitcher";
 
 const Footer = () => {
   return (
-    <footer className="footer border-t px-5 py-3">
-      <div className="flex flex-col sm:flex-row justify-between items-center">
-        <div className="flex h-7 justify-start items-center space-x-5">
-          <Divider orientation="vertical" />
-          <div className="flex justify-center items-center">
-            <Link href="tel:+18187266444">
-              <BsFillTelephoneFill />
+    <footer className="footer border-t px-5 py-4">
+      <div className="flex justify-between items-start">
+        <div className="flex flex-col items-start">
+          <div className="flex h-7 justify-start items-center space-x-5">
+            <Divider orientation="vertical" />
+            <div className="flex justify-center items-center">
+              <Link href="tel:+18187266444">
+                <BsFillTelephoneFill />
+              </Link>
+              <Link href="tel:+18187266444">
+                {/* <p className="sm:hidden">+1818-726-6444</p> */}
+              </Link>
+            </div>
+            <Divider orientation="vertical" />
+            <Link href="https://github.com/saro9588">
+              <FaGithub />
             </Link>
-            <Link href="tel:+18187266444">
-              {/* <p className="sm:hidden">+1818-726-6444</p> */}
+            <Divider orientation="vertical" />
+            <Link href="https://www.linkedin.com/in/saro-babikian-16322aa7">
+              <FaLinkedin />
             </Link>
+            <Divider orientation="vertical" />
+            <div className="flex justify-left items-center space-x-5">
+              <Link href="mailto:babikiansaro@gmail.com">
+                <MdEmail />
+              </Link>
+            </div>
+            <Divider orientation="vertical" />
           </div>
-          <Divider orientation="vertical" />
-          <Link href="https://github.com/saro9588">
-            <FaGithub />
-          </Link>
-          <Divider orientation="vertical" />
-          <Link href="https://www.linkedin.com/in/saro-babikian-16322aa7">
-            <FaLinkedin />
-          </Link>
-          <Divider orientation="vertical" />
-          <div className="flex justify-left items-center space-x-5">
-            <Link href="mailto:babikiansaro@gmail.com">
-              <MdEmail />
-            </Link>
-          </div>
-          <Divider orientation="vertical" />
-        </div>
-        <div>
-          <div className="flex items-center space-x-1">
+          <div className="flex items-center space-x-1 mt-2">
             <FaRegCopyright />
             <p>Saro Babikian 2024</p>
           </div>
