3de81b7dbeb55cfe08d51f9fade4116886baa6b3
Saro <babikiansaro@gmail.com>
2024-09-10 10:58:49 -0700

    style-home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Image } from "@nextui-org/react";
 import Link from "next/link";
-import { CgEnter } from "react-icons/cg";
 
 export default function Home() {
   return (
@@ -11,43 +10,41 @@ export default function Home() {
           <div className="child flex justify-center">
             <p className="">
               I am a LA based software developer with experience in HTML, CSS,
-              JavaScript, Java & Spring Boot and the MERN stack. I am a recent graduate of the MIT
-              xPro Full-Stack Software Development bootcamp. I have built apps, websites & REST APIs using the MERN stack & Spring Boot 3. In addition, I create and host
-              WordPress websites.
+              JavaScript, Java & Spring Boot and the MERN stack. I am a recent
+              graduate of the MIT xPro Full-Stack Software Development bootcamp.
+              I have built apps, websites & REST APIs using the MERN stack &
+              Spring Boot 3. In addition, I create and host WordPress websites.
             </p>
           </div>
           <div className="child gird grid-cols-1 md:grid-cols-1 md:justify-self-center ">
             <div className="flex justify-center">
-            <Link href="/projects">
-              <Image
-                alt="empty html tag"
-                className="w-3/4 h-auto hover:scale-105 transition-transform duration-300 mx-auto"
-                src="/dev.png"
-              />
+              <Link href="/projects">
+                <Image
+                  alt="empty html tag"
+                  className="w-3/4 h-auto hover:scale-105 transition-transform duration-300 mx-auto"
+                  src="/dev.png"
+                />
               </Link>
-              </div>
-            <div className="flex justify-center md:mt-0"><p className="animate-flash">Check out my code!</p></div>
+            </div>
+            <div className="flex justify-center md:mt-0">
+              <p className="animate-flash">Check out my code!</p>
+            </div>
           </div>
         </div>
-<div className="custom-shape-divider-bottom-1725737973">
-  <svg
-    data-name="Layer 1"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1200 120"
-    preserveAspectRatio="none"
-  >
-    <path
-      d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z"
-      className="shape-fill"
-    >
-    </path>
-  </svg>
-</div>
-
+        <div className="custom-shape-divider-bottom-1725737973">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
         <div className="parent custom-blue">
-          <div className="child">
-            <Image alt="" src="/guitar.JPG" />
-          </div>
           <div className="child flex items-center">
             <div>
               <p>
@@ -64,12 +61,16 @@ export default function Home() {
                   className="w-full h-full hover:scale-105 transition-transform duration-300"
                 />
               </Link>
-              <div className="pl-7"><p className="animate-flash">Click me!</p></div>
+              <div className="pl-7">
+                <p className="animate-flash">Click me!</p>
+              </div>
             </div>
+          </div>
+          <div className="child">
+            <Image alt="" src="/guitar.JPG" />
           </div>
         </div>
       </div>
     </div>
-
   );
 }
